94fd697344bc4f8182219df444b68905789f4af1
imranyy <imranmatano6@gmail.com>
2023-01-07 02:29:54 +0300

    working on openai -api-2

--- controllers/AI.js
+++ controllers/AI.js
@@ -7,23 +7,35 @@ const openai=new OpenAIApi(configuration);
 
 const Ask=async(req,res)=>{
     try {
-        const {prompt}=req.body
         const model="chatgpt";
-        openai.completions.create({
+        const response=await openai.createCompletion({
             engine:model,
-            prompt,
-            max_tokens:2048,
+            prompt:req.body.prompt,
             n:1,
+            max_tokens:2048,
             stop:'.',
             temperature:0.5,
-        },(error,response)=>{
-            if(error){
-                res.send(error)
-            }else{
-                res.send(response.choices[0].text)
-            }
         })
-        res.status(200).send('ok')
+        const answer=response.data.choices[0].text;
+        res.status(200).send(answer);
+
+        // const {prompt}=req.body
+        // const model="chatgpt";
+        // openai.completions.create({
+        //     engine:model,
+        //     prompt,
+        //     max_tokens:2048,
+        //     n:1,
+        //     stop:'.',
+        //     temperature:0.5,
+        // },(error,response)=>{
+        //     if(error){
+        //         res.send(error)
+        //     }else{
+        //         res.send(response.choices[0].text)
+        //     }
+        // })
+        // res.status(200).send('ok')
     } catch (error) {
         res.status(500).send(error.message)
     }
