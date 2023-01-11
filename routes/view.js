cc2997bd9627453e505fa4a59b6137619ab1156e
imranyy <imranmatano6@gmail.com>
2023-01-11 21:19:48 +0300

    starting

--- routes/view.js
+++ routes/view.js
@@ -3,73 +3,15 @@ views=express.Router();
 
 //view routes
 views.get('/',(req,res)=>{
-    res.render('index',{title:'Landing page',button:{name:'Get Diagnosed',id:'get-diagnosed'},js:'js/app.js',paths:[
-        {
-            id:1,
-            name:'Laboratory',
-            url:'/laboratory',
-            title:"Get diagnosed"
-        },
-        {
-            id:2,
-            name:'Facilities',
-            url:'/facilities',
-            title:"Local Facilities around you"
-        }
-    ]})
+    res.render('playground',{title:'Laboratory',js:'js/playground.js'})
 });
-views.get('/laboratory',(req,res)=>{
-    res.render('playground',{title:'Laboratory',js:'js/playground.js',button:{name:'Get Diagnosed',id:'get-diagnosed'},paths:[
-        {
-            id:1,
-            name:'Home',
-            url:'/',
-            title:"Landing page"
-        },
-        {
-            id:2,
-            name:'Facilities',
-            url:'/facilities',
-            title:"Local Facilities around you"
-        }
-    ]})
-});
-views.get('/facilities',(req,res)=>{
-    res.render('facilities',{title:'Facilities',js:'js/facilities.js',button:{name:'Get Diagnosed',id:'get-diagnosed'},paths:[
-        {
-            id:1,
-            name:'Home',
-            url:'/',
-            title:"Landing page"
-        },
-        {
-            id:2,
-            name:'Laboratory',
-            url:'/laboratory',
-            title:"Get Diagnosed"
-        }
-    ]})
-});
-
+ 
 //api routes
 views.use('/api',require('./API'));
 
 //rendering not found page
 views.use((req,res)=>{
-    res.status(404).render('notfound',{title:'Not found',js:'js/notfound.js',button:{name:'Get Diagnosed',id:'get-diagnosed'},paths:[
-        {
-            id:1,
-            name:'Home',
-            url:'/',
-            title:"Landing page"
-        },
-        {
-            id:2,
-            name:'Facilities',
-            url:'/facilities',
-            title:"Local Facilities around you"
-        }
-    ]})
+    res.status(404).render('notfound',{title:'Not found',js:'js/notfound.js'})
 })
 
 module.exports=views
